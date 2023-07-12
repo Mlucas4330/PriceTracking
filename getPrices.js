a7be16f1cb91ab0b7786534284093c5064ddf878
Lucas Medeiros <mlucas4330@gmail.com>
2023-07-11 22:48:55 -0300

    test 2

--- getPrices.js
+++ getPrices.js
@@ -2,11 +2,11 @@ import puppeteer from 'puppeteer';
 
 const url = 'https://www.decolar.com/trip/accommodations/detail/PC69a2161822cc47dabfd56a0e0b50de8828151883?hotel_product_id=H1&flow=FH&abcv=YWJjdi1mb3JjZWRHRFM9ZmFsc2UmYWJjdi1mb3JjZWRGaW5hbD1mYWxzZQ&searchParams=RkgvQ0lUXzU4MjIvQ0lUXzYzODEvMjAyMy0xMS0xNi8yMDIzLTExLTE4L0NJVF82MzgxLzIwMjMtMTEtMTYvMjAyMy0xMS0xOC8zfEgxOkgsRjA6RixYUzpYUw%3D%3D&stepNum=0&throughResults=true&searchId=a443db3c-dd05-4350-a6a2-30b9efe6c9cd';
 
-const getPrices = () => {
-    const browser = puppeteer.launch({ headless: "new" });
-    const page = browser.newPage();
+const getPrices = async () => {
+    const browser = await puppeteer.launch({ headless: "new" });
+    const page = await browser.newPage();
 
-    page.goto(url);
+    await page.goto(url);
 
     const prices = page.evaluate(() => {
         const totalPrice = document.querySelector(".tertiary-price");
@@ -20,9 +20,9 @@ const getPrices = () => {
         }
 
     })
-    browser.close();
+    await browser.close();
 
-    return prices;
+    return Promise.resolve(prices);
 }
 
 export default getPrices;
